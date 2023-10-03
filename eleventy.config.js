9d0020978698a2875012b16fcefd0f955a48648b
Francis Storr <francis.storr@intel.com>
2023-10-02 20:56:10 -0700

    Use base plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
 
+  const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("src/styles/");
   // Return your Object options:
   return {
