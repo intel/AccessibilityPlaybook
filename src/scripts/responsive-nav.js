1be1dd752392806589cb4af14efc09420b6738ef
Francis Storr <francis.storr@intel.com>
2023-10-23 11:30:09 -0700

    Adding some javascript to get around Firefox’s lack of support for CSS :has selector

--- src/scripts/responsive-nav.js
+++ src/scripts/responsive-nav.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(e){
-	const navTaggleBtn = document.querySelector(".nav--site-toggle");
+	let navTaggleBtn = document.querySelector(".nav--site-toggle");
+	let responsiveElmsToHide = document.querySelectorAll(".skip-link, header, main, footer");
 	
 	navTaggleBtn.addEventListener("click", toggleSiteNav, false);
 
@@ -7,9 +8,31 @@ document.addEventListener("DOMContentLoaded", function(e){
 		let elm = e.currentTarget;
 		if (elm.getAttribute("aria-expanded") === "false"){
 			elm.setAttribute("aria-expanded", "true");
+			for(let i=0;i<responsiveElmsToHide.length;i++){
+				responsiveElmsToHide[i].setAttribute("hidden", "");
+			}
 		}
 		else{
 			elm.setAttribute("aria-expanded", "false");
+			for(let i=0;i<responsiveElmsToHide.length;i++){
+				responsiveElmsToHide[i].removeAttribute("hidden");
+			}
 		}
 	}
+
+	let ro = new ResizeObserver(entries => {
+		for (let entry of entries) {
+			const cr = entry.contentRect;
+			if(cr.width <= 560){
+				document.querySelector("body").classList.add("responsive-nav");
+			}
+			else{
+				document.querySelector("body").classList.remove("responsive-nav");
+			}
+		}
+	});
+	
+	// Observe one or multiple elements
+	ro.observe(document.querySelector("body"));
+
 });
