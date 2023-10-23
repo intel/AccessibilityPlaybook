77448627666656db25e19359a1e4164286573733
Francis Storr <francis.storr@intel.com>
2023-10-23 12:13:49 -0700

    Passthrough for images

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("src/styles/");
   eleventyConfig.addPassthroughCopy("src/scripts/*");
+  eleventyConfig.addPassthroughCopy("src/images/*");
   // Return your Object options:
   return {
     pathPrefix: "/AccessibilityPlaybook-fs/",
