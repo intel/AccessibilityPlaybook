865b4a003715b35bfc777419ae636848f1a419ce
Francis Storr <francis.storr@intel.com>
2023-10-24 09:57:27 -0700

    Add support for Escape key closing responsive nav

--- src/scripts/responsive-nav.js
+++ src/scripts/responsive-nav.js
@@ -7,17 +7,26 @@ document.addEventListener("DOMContentLoaded", function(e){
 	function toggleSiteNav(e){
 		let elm = e.currentTarget;
 		if (elm.getAttribute("aria-expanded") === "false"){
-			elm.setAttribute("aria-expanded", "true");
-			for(let i=0;i<responsiveElmsToHide.length;i++){
-				responsiveElmsToHide[i].setAttribute("hidden", "");
-			}
+			openNav(e);
 		}
 		else{
-			elm.setAttribute("aria-expanded", "false");
-			for(let i=0;i<responsiveElmsToHide.length;i++){
-				responsiveElmsToHide[i].removeAttribute("hidden");
-			}
+			closeNav(e);
 		}
+	}
+
+	function openNav(){
+		navTaggleBtn.setAttribute("aria-expanded", "true");
+		for(let i=0;i<responsiveElmsToHide.length;i++){
+			responsiveElmsToHide[i].setAttribute("hidden", "");
+		}
+	}
+
+	function closeNav(){
+		navTaggleBtn.setAttribute("aria-expanded", "false");
+		for(let i=0;i<responsiveElmsToHide.length;i++){
+			responsiveElmsToHide[i].removeAttribute("hidden");
+		}
+		navTaggleBtn.focus();
 	}
 
 	let ro = new ResizeObserver(entries => {
@@ -25,12 +34,20 @@ document.addEventListener("DOMContentLoaded", function(e){
 			const cr = entry.contentRect;
 			if(cr.width <= 560){
 				document.querySelector("body").classList.add("responsive-nav");
+				window.addEventListener("keyup", closeNavOnEsc, false);
 			}
 			else{
 				document.querySelector("body").classList.remove("responsive-nav");
+				window.removeEventListener("keyup", closeNavOnEsc, false);
 			}
 		}
 	});
+
+	function closeNavOnEsc(e){
+		if(e.key === "Escape"){
+			closeNav();
+		}
+	}
 	
 	// Observe one or multiple elements
 	ro.observe(document.querySelector("body"));
